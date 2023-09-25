27f101c87ad56379222428dcf3cf480a91bc9eb6
CamiloCastiblanco <69698380+CamiloCastiblanco@users.noreply.github.com>
2023-09-25 15:53:59 -0500

    Controller Ready

---
+++ src/main/java/co/edu/escuelaing/ieti/HelloApplication.java
@@ -0,0 +1,15 @@
+package co.edu.escuelaing.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.ieti.controller"})
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+}
---
+++ src/main/java/co/edu/escuelaing/ieti/controller/HelloController.java
@@ -0,0 +1,22 @@
+package co.edu.escuelaing.ieti.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Camilo Castiblanco
+ */
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> GetResource() {
+        return new ResponseEntity<>("Hello Camilo Castiblanco!", HttpStatus.ACCEPTED);
+    }
+
+}
